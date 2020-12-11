a4f94b6c09b912fbe22a7012c510813966003f4e
quietpochatok <bosroin@gmail.com>
2020-12-11 20:06:51 +0500

    delete action index, new, create in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  def index
-    @users = User.all
-  end
-
   # GET /users/1
   def show
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
   # GET /users/1/edit
   def edit
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
-    else
-      render :new
-    end
   end
 
   # PATCH/PUT /users/1
@@ -40,10 +19,10 @@ class UsersController < ApplicationController
   end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
-  end
+  # def destroy
+  #   @user.destroy
+  #   redirect_to users_url, notice: 'User was successfully destroyed.'
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -53,6 +32,7 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.fetch(:user, {})
+      #params.fetch(:user, {})
+      params.require(:user).permit(:name, :email)
     end
 end
