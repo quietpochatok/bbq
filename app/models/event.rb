68ff18f0f9c164bbe407400d22312ffd62b877d7
quietpochatok <bosroin@gmail.com>
2020-12-11 20:19:02 +0500

    add belongs_to :user in model Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :user
+  # В Rails 5 связи belongs_to :user валидируются :user  по умолчанию
   # Валидируем заголовок, он не может быть длиннее 255 букв
   validates :title, presence: true, length: {maximum: 255}
   # У события должны быть заполнены место и время
