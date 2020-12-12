649bfc8518e165f834d1065c0e45c0857313575a
quietpochatok <bosroin@gmail.com>
2020-12-12 22:18:03 +0500

    change views/devise/registrations/new with style bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,28 @@
 module ApplicationHelper
   # здесь только доступно во вьюхах!
   def bootstrap_flash(opts = {})
-  flash.each do |msg_type, message|
-    concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do
-      concat content_tag(:button, '×', class: "close", data: { dismiss: 'alert' })
-      concat message
-    end)
-  end
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do
+        concat content_tag(:button, '×', class: "close", data: { dismiss: 'alert' })
+        concat message
+      end)
+    end
   nil
-end
+  end
 
-def user_avatar(user)
-asset_pack_path('media/images/mangal.png')
-end
+  def user_avatar(user)
+  asset_pack_path('media/images/mangal.png')
+  end
+
+  def minimum_password_length
+    if @minimum_password_length
+     "Password #{@minimum_password_length} characters min"
+    end
+  end
+
+
 private
-def bootstrap_class_for(flash_type)
-  { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-success" }.stringify_keys[flash_type.to_s] || flash_type.to_s
-end
+  def bootstrap_class_for(flash_type)
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-success" }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
 end
