e6ba17faf07415598ae86bfecd73f9a26e57689e
quietpochatok <bosroin@gmail.com>
2020-12-14 01:21:52 +0500

    fix db t.references :user

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2020_12_13_143221) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.string "user_name"
-    t.integer "event_id", null: false
-    t.integer "user_id", null: false
+    t.integer "event_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_comments_on_event_id"
