05bea35d1d1f6f4dadc7fcceb85feddcd336e129
quietpochatok <bosroin@gmail.com>
2020-12-20 20:56:19 +0500

    refactor: change name method into helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
     )
   end
 
-  def current_user_can_subs?(event)
+  def current_user_can_subscribe?(event)
     event.user == current_user
   end
 
