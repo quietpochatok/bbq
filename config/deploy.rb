7d28d1c8a10faf44369b3acd25525882652c4d28
quietpochatok <bosroin@gmail.com>
2021-01-05 18:02:38 +0500

    Add files for send to server & upd gitignore

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_url, 'git@github.com:quietpochatok/bbq.git'
 set :deploy_to, '/home/deploy/apps/bbq'
 
 # Файлы, которые лежат вне репозитория
-append :linked_files, '.env'
+append :linked_files, '.env', 'config/master.key', 'config/database.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
 # Default branch is :master
